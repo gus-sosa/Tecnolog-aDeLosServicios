955e2000f2a3144da685e4811b0f1ca676af0926
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-05-23 23:13:47 -0500

    Adding an auxiliar project that helped for scaffolding the images links for the carousel sections

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string segmentPath = @"assets\img\services\americano";
+            string path = $@"C:\Users\gcuervo\Desktop\dd\{segmentPath}";
+            string segmentTemplate = $@"<div class=""item"">
+    <img src = ""{segmentPath.Replace('\\', '/')}/{{0}}"" >
+</div>";
+            foreach (var fileName in Directory.EnumerateFiles(path))
+                Console.WriteLine(segmentTemplate, Path.GetFileName(fileName));
+        }
+    }
+}
